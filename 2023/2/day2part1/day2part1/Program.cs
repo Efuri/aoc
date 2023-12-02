ccc848032d078f0c93aedaca6a6dfc2900f9f9fb
Efuri <33378439+Efuri@users.noreply.github.com>
2023-12-02 21:51:35 +0100

    dag 2

---
+++ 2023/2/day2part1/day2part1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace day2part1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = File.ReadAllLines(@"C:\Users\Erikf\OneDrive\Dokument\MEGAsync\prog\aoc\2023\2\input1.txt");
+            int games = 0;
+            int red = 0;
+            int green = 0;
+            int blue = 0;
+
+            foreach (string line in input)
+            {
+                int colonIndex = line.IndexOf(':');
+                string withoutbs = line.Substring(colonIndex + 1, line.Length - colonIndex - 1);
+                withoutbs = withoutbs.Replace(" ", "");
+                string[] splitted = withoutbs.Split(',', ';');
+                foreach (string line2 in splitted)
+                {
+                    if (line2.Contains("red"))
+                    {
+                        red += int.Parse(line2.Substring(0, 1));
+                    }
+                    if (line2.Contains("green"))
+                    {
+                        green += int.Parse(line2.Substring(0, 1));
+                    }
+                    if (line2.Contains("blue"))
+                    {
+                        blue += int.Parse(line2.Substring(0, 1));
+                    }
+                }
+                if (red <= 12 && blue <= 14 && green <= 13)
+                {
+                    games++;
+                }
+                red = 0;
+                green = 0;
+                blue = 0;
+            }
+            Console.WriteLine(games);
+        }
+    }
+}
---
+++ 2023/2/debug/debug/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace debug
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = File.ReadAllLines(@"C:\Users\Erikf\OneDrive\Dokument\MEGAsync\prog\aoc\2023\2\input1.txt");
+            int games = 0;
+            int red = 0;
+            int green = 0;
+            int blue = 0;
+
+            foreach (string line in input)
+            {
+                int colonIndex = line.IndexOf(':');
+                string withoutbs = line.Substring(colonIndex + 1, line.Length-colonIndex-1);
+
+                withoutbs = withoutbs.Replace(" ", "");
+                string[] splitted = withoutbs.Split(',', ';');
+                foreach (string line2 in splitted)
+                {
+                    Console.WriteLine(line2);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
